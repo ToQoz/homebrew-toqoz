663e95c2f45ffe4a29bb7179fa3d2bfb90812028
Takatoshi Matsumoto <toqoz403@gmail.com>
2015-03-02 12:14:21 +0900

    tmux: use HEAD

--- tmux.rb
+++ tmux.rb
@@ -1,31 +1,16 @@
 require 'formula'
 
 class Tmux < Formula
-  homepage 'http://tmux.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/tmux/tmux/tmux-1.9/tmux-1.9a.tar.gz'
-  sha1 '815264268e63c6c85fe8784e06a840883fcfc6a2'
+  url 'git://git.code.sf.net/p/tmux/tmux-code'
 
-  bottle do
-    cellar :any
-    revision 1
-    sha1 "5a5e180e33339671bc8c82ed58c26862da037f30" => :yosemite
-    sha1 "6092f92f5cd7eeb6ddf3b555cd4e655c4c85e826" => :mavericks
-    sha1 "981c8c199a2ea3df18b6651205b4616459ae1f8c" => :mountain_lion
-  end
-
-  head do
-    url 'git://git.code.sf.net/p/tmux/tmux-code'
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on 'pkg-config' => :build
   depends_on 'libevent'
 
   def install
-    system "sh", "autogen.sh" if build.head?
+    system "sh", "autogen.sh"
 
     ENV.append "LDFLAGS", '-lresolv'
     system "./configure", "--disable-dependency-tracking",
