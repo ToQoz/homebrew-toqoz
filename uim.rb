3b9a0f15f91f7f58c2d13b857853e808e9f5b75f
Takatoshi Matsumoto <toqoz403@gmail.com>
2015-03-02 12:03:20 +0900

    uim: 1.6.0 -> 1.8.6. add intltool to deps

--- uim.rb
+++ uim.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Uim < Formula
   homepage 'http://code.google.com/p/uim/'
-  url 'https://uim.googlecode.com/files/uim-1.6.0.tar.bz2'
-  sha1 'd27f2ca8136da0702c82f0522911d06b2b8f8ea7'
+  url 'https://uim.googlecode.com/files/uim-1.8.6.tar.bz2'
+  sha1 '43b9dbdead6797880e6cfc9c032ecb2d37d42777'
 
   deprecated_option "with-x" => "with-x11"
 
   depends_on 'pkg-config' => :build
+  depends_on 'intltool' => :build
   depends_on 'gettext'
   depends_on :x11 => :optional
 
