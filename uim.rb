771b1ff00780806de00df4f8be28e056138f7939
Takatoshi Matsumoto <toqoz403@gmail.com>
2015-03-02 11:58:55 +0900

    uim: set optimization level via homebrew's helper

--- uim.rb
+++ uim.rb
@@ -12,8 +12,22 @@ class Uim < Formula
   depends_on :x11 => :optional
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    # - Optimazation will raise segv on build.
+    # - ENV['CFLAGS'] will be overwritten by ENV['HOMEBREW_OPTIMIZATION_LEVEL']
+    #   https://github.com/Homebrew/homebrew/blob/6dd602f073a97094839d05d2126d944434b5c752/Library/ENV/4.3/cc#L187-L199
+    # - ENV.{O3, O2, O1, O0, Os} sets HOMEBREW_OPTIMIZATION_LEVEL
+    #   https://github.com/Homebrew/homebrew/blob/b5ab4744bbd88dc65c8e6fb0f54b90dc8f535611/Library/Homebrew/extend/ENV/super.rb#L45
+    ENV.O0
+
+    cargs = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}",
+    ]
+    cargs << "--without-x" if build.without? "x11"
+
+    system "./configure", *cargs
+
     system "make install"
   end
 end
