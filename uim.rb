b0abc209dd4f4f5e956837eef42e8087d8054a81
Takatoshi Matsumoto <toqoz403@gmail.com>
2015-03-02 11:57:28 +0900

    uim: add x11 to deps. and treat it as optional

--- uim.rb
+++ uim.rb
@@ -5,8 +5,11 @@ class Uim < Formula
   url 'https://uim.googlecode.com/files/uim-1.6.0.tar.bz2'
   sha1 'd27f2ca8136da0702c82f0522911d06b2b8f8ea7'
 
+  deprecated_option "with-x" => "with-x11"
+
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
+  depends_on :x11 => :optional
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
