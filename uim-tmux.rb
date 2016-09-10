8501b6ff4b379efa0b28288aeab851f396afd744
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-09-10 18:51:34 +0900

    uim-tmux: use sha256 instead of sha1

--- uim-tmux.rb
+++ uim-tmux.rb
@@ -3,7 +3,7 @@ require 'formula'
 class UimTmux < Formula
   homepage 'https://github.com/ToQoz/uim-tmux'
   url 'https://github.com/ToQoz/uim-tmux/archive/v0.0.1.tar.gz'
-  sha1 'dc521f75fae78b38dca0f33cfed97f19c8ee5bde'
+  sha256 'a77a8f8c26d1b7e8cb46fa47f654a8b22aebd97ef4f1400bdf077d5e87f3fdad'
 
   head 'https://github.com/ToQoz/uim-tmux.git'
 
